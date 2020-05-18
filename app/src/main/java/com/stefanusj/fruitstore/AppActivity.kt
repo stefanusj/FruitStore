780085d212d391253088da327ef88ae99121edaa
Stefanus Julianto <stefanusj3@gmail.com>
2020-05-19 04:37:09 +0700

    Initial commit

---
+++ app/src/main/java/com/stefanusj/fruitstore/AppActivity.kt
@@ -0,0 +1,10 @@
+package com.stefanusj.fruitstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/stefanusj/fruitstore/ui/fruit/FruitViewModel.kt
@@ -0,0 +1,7 @@
+package com.stefanusj.fruitstore.ui.fruit
+
+import androidx.lifecycle.ViewModel
+
+class FruitViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
